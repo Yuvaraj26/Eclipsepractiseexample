d724f2f2a0b40bf9e4f490d831ec827a4872c666
Yuvaraj26 <yuvi1064@gmail.com>
2019-12-05 13:37:19 +0530

    New program

--- Samplepro/src/main/java/Firstone/Samplepro/Driverexamples.java
+++ Samplepro/src/main/java/Firstone/Samplepro/Driverexamples.java
@@ -1,9 +1,23 @@
 package Firstone.Samplepro;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Driverexamples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","/Users/yuvarajk/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.childrensplace.com");
+		String e=driver.getCurrentUrl();
+		String f=driver.getPageSource(); 
+		String g=driver.getTitle();
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+		
 
 	}
 
