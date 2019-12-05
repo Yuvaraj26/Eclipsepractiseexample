6fd0090a363d2ec8b8f47b308abd3793c6bab77c
Yuvaraj26 <yuvi1064@gmail.com>
2019-12-05 12:49:46 +0530

    Push to repo

--- Samplepro/src/main/java/Firstone/Samplepro/Cookiesexample.java
+++ Samplepro/src/main/java/Firstone/Samplepro/Cookiesexample.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Cookiesexample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method
 		System.setProperty("webdriver.chrome.driver", "/Users/yuvarajk/Downloads/chromedriver");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.childrensplace.com/us/home");
